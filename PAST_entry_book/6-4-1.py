d8c48037f400bfbc090461d1a23f005bdbabcc60
lowlevelengineer <lowlevelengineer@gmail.com>
2024-01-04 23:53:58 +0900

    commit

--- PAST_entry_book/6-4-1.py
+++ PAST_entry_book/6-4-1.py
@@ -3,10 +3,11 @@ h = list(map(int, input().split()))
 
 cost = [0]*N
 
+# 初期化
 cost[0]=0
-
 cost[1]=cost[0]+abs(h[0]-h[1])
 
+# それ以降の足場はジャンプ元が2通りあるため、コストが小さい方を採用する
 for i in range(2,N):
     cost[i] = min(cost[i-1]+abs(h[i-1]-h[i]), cost[i-2] + abs(h[i-2]-h[i]))
 
