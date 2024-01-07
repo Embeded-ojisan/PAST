480207b45acff8cb18f724abd30ba5e5cdda7f75
lowlevelengineer <lowlevelengineer@gmail.com>
2024-01-07 18:07:17 +0900

    commit

--- AtCoder_Intro/6-3-2.py
+++ AtCoder_Intro/6-3-2.py
@@ -7,3 +7,16 @@ def bfs(G, s):
 
     que.put(s)
     dist[s] = 0
+
+    while not que.empty():
+        v = que.get()
+
+        for v2 in G[v]:
+            if dist[v2] != -1:
+                continue
+
+            que.put(v2)
+
+            dist[v2] = dist[v] + 1
+    
+    return dist
