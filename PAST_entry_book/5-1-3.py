222d020d88c61d6acf9d0b8659e1109cbe20cbd7
lowlevelengineer <lowlevelengineer@gmail.com>
2024-01-08 18:22:05 +0900

    commit

--- PAST_entry_book/5-1-3.py
+++ PAST_entry_book/5-1-3.py
@@ -1,22 +1,11 @@
 N = int(input())
 
-S = []
+S = [list(input()) for _ in range(N)]
 
+for I in range(N-2, -1, -1):
+    for J in range(1, 2*N-3):
+        if S[I][J] == '#' and (S[I+1][J-1] == 'X' or S[I+1][J] == 'X' or S[I+1][J+1] == 'X'):
+            S[I][J] = 'X'
+        
 for i in range(0, N):
-    si = input()
-    si = list(si)
-    S.append(si)
-
-for i in range(N-2, -1, -1):
-    for j in range(1, 2*N-1):
-        if S[i][j] == '#':
-            if S[i+1][j-1] == 'X':
-                S[i][j] = 'X'
-            if S[i+1][j] == 'X':
-                S[i][j] = 'X'
-            if S[i+1][j+1] == 'X':
-                S[i][j] = 'X'
-
-for i in range(0, N):
-    S[i] = ''.join(S[i])
-    print(S[i])
+    print(''.join(S[i]))
