f32dd51fa05782942dbf38054ecfea1078223abf
lowlevelengineer <lowlevelengineer@gmail.com>
2024-01-08 12:10:52 +0900

    commit

--- PAST_entry_book/5-1-1.py
+++ PAST_entry_book/5-1-1.py
@@ -1,39 +1,40 @@
+# 前処理
+A = [0, 0, 0] * 3
+B = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
-A = []
+Result = "No"
 
-for _ in range(0, 3):
-    row = list(map(int, input().split()))
-    A.append(row)
+# ビンゴカードを取り込む
+for i in range(3):
+    A[i] = list(map(int, input().split()))
 
-M = [[False, False, False], [False, False, False], [False, False, False]]
-
+# 印を付ける
 N = int(input())
-
-for _ in range(0, N):
+for n in range(N):
     b = int(input())
 
-    for i in range(0, 3):
-        for j in range(0, 3):
+    for i in range(3):
+        for j in range(3):
             if A[i][j] == b:
-                M[i][j] = True
+                B[i][j] = 1
+                break
 
-bingo = False
+# ビンゴがあるかをチェックする
+## 縦チェック
+if (B[0][0] and B[1][0] and B[2][0]) \
+    or (B[0][1] and B[1][1] and B[2][1]) \
+    or (B[0][2] and B[1][2] and B[2][2]):
+        Result = "Yes"
 
-for i in range(0, 3):
-    if M[i][0] and M[i][1] and M[i][2]:
-        bingo = True
+## 横チェック
+if (B[0][0] and B[0][1] and B[0][2]) \
+    or (B[1][0] and B[1][1] and B[1][2]) \
+    or (B[2][0] and B[2][1] and B[2][2]):
+        Result = "Yes"
 
-for i in range(0, 3):
-    if M[0][i] and M[1][i] and M[2][i]:
-        bingo = True
+## 斜めチェック
+if (B[0][0] and B[1][1] and B[2][2]) \
+    or (B[0][2] and B[1][1] and B[2][0]):
+        Result = "Yes"
 
-if M[0][0] and M[1][1] and M[2][2]:
-    bingo = True
-
-if M[0][2] and M[1][1] and M[2][0]:
-    bingo = True
-
-if bingo:
-    print("Yes")
-else:
-    print("No")
+print(Result)
