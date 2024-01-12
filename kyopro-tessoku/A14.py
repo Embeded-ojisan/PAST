0c229498ec7bfe5c82b5db37f415dc7a4c24d20d
lowlevelengineer <lowlevelengineer@gmail.com>
2024-01-12 23:45:25 +0900

    commit

---
+++ kyopro-tessoku/A14.py
@@ -0,0 +1,29 @@
+import bitsect
+import sys
+
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+C = list(map(int, input().split()))
+D = list(map(int, input().split()))
+
+P = []
+for x in range(N):
+    for y in range(N):
+        P.append(A[x] + B[y])
+
+
+Q = []
+for z in range(N):
+    for w in range(N):
+        Q.append(C[z] + D[w])
+
+Q.sort()
+
+for i in range(len(P)):
+    pos1 = bitsect.bitsect_left(Q, K-P[i])
+    if post < N*N and Q[pos1] == K-P[i]:
+        print("Yes")
+        sys.exit(0)
+
+print("No")
