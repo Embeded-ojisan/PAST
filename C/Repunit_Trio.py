5c91be1badb44cf135267e32fbfe8fc258420eaf
lowlevelengineer <lowlevelengineer@gmail.com>
2024-01-30 23:00:15 +0900

    commit

---
+++ C/Repunit_Trio.py
@@ -0,0 +1,10 @@
+N = int(input())
+L = 12
+r = [int("1"*(i+1)) for i in range(L)]
+
+for i in range(L):
+    for j in range(i+1):
+        for k in range(j+1):
+            N -= 1
+            if N==0:
+                print(r[i]+r[j]+r[k])
