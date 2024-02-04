ee980ae1131e3daf4e8b3e1118b44c56fa266d1a
lowlevelengineer <lowlevelengineer@gmail.com>
2024-02-04 23:21:56 +0900

    commit

---
+++ C/321-like_Searcher.py
@@ -0,0 +1,15 @@
+K = int(input())
+
+ans = []
+
+for i in range(2, 1<<10):
+    x = 0
+    for j in range(9, -1, -1):
+        if i&1<<j:
+            x*=10
+            x+=j
+    ans.append(x)
+
+ans.sort()
+
+print(str(ans[K-1]))
