1bed3384cea980594a1fce73c00f2c65617c0231
lowlevelengineer <lowlevelengineer@gmail.com>
2024-02-10 23:29:11 +0900

    commit

--- PAST_entry_book/6-4-3.py
+++ PAST_entry_book/6-4-3.py
@@ -5,7 +5,6 @@ N = int(input())
 h = list(map(int, input().split()))
 
 cost = [0]*N
-
 done = [False]*N
 
 def rec(i):
@@ -17,9 +16,9 @@ def rec(i):
     elif i == 1:
         cost[i] = rec(0)+abs(h[0]-h[1])
     else:
-        cost[i] = min(rec(i-1)+abs(h[i-1]-h[i]), rec(i-2)+abs(h[i-2]-h[i]))
+        cost[i] = min(rec(i-1) + abs(h[i-1]-h[i]), rec(i-2) + abs(h[i-2]-h[i]))
 
-    done[i] = True
+    done[i] == True
     return cost[i]
 
 print(rec(N-1))
