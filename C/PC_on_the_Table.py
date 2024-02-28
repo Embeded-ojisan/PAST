3aa869b1089aabba6a1fd0d35005e678ad0582fd
lowlevelengineer <lowlevelengineer@gmail.com>
2024-02-29 00:00:11 +0900

    c

---
+++ C/PC_on_the_Table.py
@@ -0,0 +1,13 @@
+H, W = list(map(int, input().split()))
+
+S = [input() for _ in range(H)]
+
+for i in range(H):
+    moji = ""
+    for j in range(W-1):
+        if S[i][j] == 'T' and S[i][j+1] == 'T':
+            S[i] = S[i][:j] + 'P' + S[i][j+1:]
+            S[i] = S[i][:j+1] + 'C' + S[i][j+2:]
+        moji += S[i][j]
+    moji += S[i][W-1]
+    print(moji)
