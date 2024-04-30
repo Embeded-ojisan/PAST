752ab7f0210eeca1a8160b933da1ebd8a50370c3
lowlevelengineer <lowlevelengineer@gmail.com>
2024-04-30 23:57:23 +0900

    commit

--- kyopro-tessoku/A16.py
+++ kyopro-tessoku/A16.py
@@ -1,12 +1,12 @@
 N = int(input())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
+A = [0] + [0] + list(map(int, input().split()))
+B = [0] + [0] + [0] + list(map(int, input().split()))
 
-dp = [None]*(N+1)
+dp = [0] * 100009
 dp[1] = 0
-dp[2] = A[0]
+dp[2] = A[2]
 
 for i in range(3, N+1):
-    dp[i] = min(dp[i-1]+A[i-2], dp[i-2]+B[i-3])
+    dp[i] = min(dp[i-1] + A[i], dp[i-2] + B[i])
 
 print(dp[N])
--- kyopro-tessoku/A17.py
+++ kyopro-tessoku/A17.py
@@ -1,31 +1,33 @@
 N = int(input())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
+A = [0] + [0] + list(map(int, input().split()))
+B = [0] + [0] + [0] + list(map(int, input().split()))
 
-dp = [None]*(N+1)
+dp = [0] * 100009
 dp[1] = 0
-dp[2] = A[0]
+dp[2] = A[2]
 
 for i in range(3, N+1):
-    dp[i] = min(dp[i-1]+A[i-2], dp[i-2]+B[i-3])
-
+    dp[i] = min(dp[i-1] + A[i], dp[i-2] + B[i])
 
 Answer = []
-Place = N
 
+Place = N
 while True:
     Answer.append(Place)
-    
     if Place == 1:
         break
-
-    if dp[Place-1]+A[Place-2] == dp[Place]:
+    if dp[Place-1]+A[Place] == dp[Place]:
         Place = Place - 1
     else:
-        Place = Place -2
+        Place = Place - 2
 
 Answer.reverse()
 
-Answer2 = [str(i) for i in Answer]
+moji = ""
+
 print(len(Answer))
-print(" ".join(Answer2))
+
+for i in Answer:
+    moji += str(i) + " "
+
+print(moji)
