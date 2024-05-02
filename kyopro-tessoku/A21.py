b38aebc4e25f309ef0150a1f2b5af3ba3ad2aa96
lowlevelengineer <lowlevelengineer@gmail.com>
2024-05-02 21:22:45 +0900

    commit

--- kyopro-tessoku/A21.py
+++ kyopro-tessoku/A21.py
@@ -1,33 +1,38 @@
 N = int(input())
-P = [None]*(N+1)
-A = [None]*(N+1)
 
-for i in range(1, N+1):
-    P[i], A[i] = map(int, input().split())
+P = [0]
+A = [0]
 
-dp = [ [None] * (N+1) for i in range(N+1)]
-dp[1][N] = 0
 
-for LEN in reserved(range(0, N-1)):
-    for l in range(1, N-LEN+1):
-        r = 1 + LEN
+for i in range(N):
+    ptemp, atemp = list(map(int, input().split()))
+    P.append(ptemp)
+    A.append(atemp)
+
+dp = [[0]*N for i in ragen(N)]
+
+for LEN in range(N-2, 0, -1):
+    for l in range(1, N-LEN):
+        r = l + LEN
 
         score1 = 0
-        if l >= 22 and l <= P[l-1] and P[l-1]<=r:
+        if l <= P[l-1] and P[l-1] <= r:
             score1 = A[l-1]
 
         score2 = 0
-        if r <= N-1 and l <= P[r+1] and P[r+1] <= r:
+        if l <= P[r+1] and P[r+1] <= r:
             score2 = A[r+1]
 
         if l == 1:
-            dp[l][r] = dp[1][r+1] + score2
+            dp[l][r] = dp[l][r+1]+score2
         elif r == N:
-            dp[l][r] = dp[l-1][r] + score1
+            dp[l][r] = dp[l-1][r]+score1
         else:
-            dp[l][r] = max(dp[l-1][r]+score1, dp[1][r+1] + score2)
+            dp[l][r] = max(dp[l-1][r]+score1, dp[l][r+1]+score2)
 
 Answer = 0
+
 for i in range(1, N+1):
-    Answer = max(Answer, dp[i][i])
+    Answer = max(Answer, dp[i][i]):
+
 print(Answer)
